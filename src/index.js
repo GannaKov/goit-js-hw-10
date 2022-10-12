4fdedae69e767faef0a9c445dc88b3de0e72c07c
GannaKov <zlatta2000@gmail.com>
2022-10-13 01:47:20 +0300

    peru

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const base_url = 'https://restcountries.com/v3.1/name/';
+function fetchCountries(name) {
+  const resp = fetch(`${base_url}/${name}`);
+  console.log(`${base_url}/${name}`);
+  resp.then(response => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  });
+  // .then(date => {});
+}
+console.log(fetchCountries('peru'));
