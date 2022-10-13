3387716b7b49970a68f3cafad1507949cbbda45d
GannaKov <104867435+GannaKov@users.noreply.github.com>
2022-10-13 17:07:48 +0000

    Deploying to gh-pages from @ GannaKov/goit-js-hw-10@59dc51ee5c33099aa80e16151556caf3d0464b45 🚀

--- index.664fc3f0.js
+++
@@ -1,2 +0,0 @@
-console.log(function(o){const t=fetch(`https://restcountries.com/v3.1/name//${o}`);console.log(`https://restcountries.com/v3.1/name//${o}`),t.then((o=>{if(!o.ok)throw new Error;return o.json()}))}("peru"));
-//# sourceMappingURL=index.664fc3f0.js.map
--- index.bb91096e.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,n,c="https://restcountries.com/v3.1/name/";console.log((o="peru",n=fetch("".concat(c,"/").concat(o)),console.log("".concat(c,"/").concat(o)),void n.then((function(o){if(!o.ok)throw new Error;return o.json()}))))}();
-//# sourceMappingURL=index.bb91096e.js.map
