87c87282745d413308e49ccdb703331a4e5cb740
GannaKov <zlatta2000@gmail.com>
2022-10-14 16:33:08 +0300

    notifz ooops

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ function fetchCountries(name) {
     `${base_url}/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
+      Notify.failure('Oops, there is no country with that name');
       throw new Error(response.status);
     }
     return response.json();
